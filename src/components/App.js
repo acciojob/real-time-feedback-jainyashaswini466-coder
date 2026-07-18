47a62d1a415149ee237b8992656626770844ab0b
jainyashaswini466-coder <jainyashaswini466@gmail.com>
2026-07-18 19:43:02 +0530

    a1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+// import './../styles/App.css';
+import TodoList from "./TodoList";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+  const [todos,setTodos] = useState([{id:"1", text:"Learn React",completed:false},{id:"2", text:"Build a React app",completed:false},{id:"3", text:"Deploy the app",completed:false}])
+  function handleComplete(id){
+    setTodos(todos.map(todo=>
+       todo.id==id ? {...todo,completed:true} : todo
+    ))
+  }
+  return (
+    <div>
+      <h1>Parent Component</h1>
+
+        <TodoList  todos={todos} handleComplete={handleComplete}/>
+    </div>
+  )
 }
 
 export default App
